e70cb3aeeb9a30b7d2d00a674d1953abe0c7223a
quocnb <quocnb.vnu@gmail.com>
2018-02-27 17:06:20 +0900

    Add Project for test

---
+++ FastlaneTestUITests/FastlaneTestUITests.swift
@@ -0,0 +1,37 @@
+//
+//  FastlaneTestUITests.swift
+//  FastlaneTestUITests
+//
+//  Created by Quoc Nguyen on 2/26/18.
+//  Copyright © 2018 Quoc Nguyen. All rights reserved.
+//
+
+import XCTest
+import FastlaneTest
+
+class FastlaneTestUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        let app = XCUIApplication()
+        app.launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testClickButton() {
+        let app = XCUIApplication()
+        app.buttons["Next"].tap()
+        let backButton = app.navigationBars["UIView"].buttons["Back"]
+        backButton.tap()
+    }
+    
+}
